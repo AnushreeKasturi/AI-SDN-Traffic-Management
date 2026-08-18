8d523b2b880f4466f5dd5482125e0a655d4c6c25
Anushree Kasturi <anushree@Anushree.localdomain>
2026-08-18 06:17:15 +0000

    Milestone 3: Implement learning switch

--- controller/adaptive_controller.py
+++ controller/adaptive_controller.py
@@ -1,7 +1,11 @@
 from os_ken.base import app_manager
 from os_ken.controller import ofp_event
 from os_ken.controller.handler import CONFIG_DISPATCHER
+from os_ken.controller.handler import MAIN_DISPATCHER
 from os_ken.controller.handler import set_ev_cls
+from os_ken.lib.packet import packet
+from os_ken.lib.packet import ethernet
+from os_ken.lib.packet import ether_types
 
 
 class AdaptiveController(app_manager.OSKenApp):
@@ -15,6 +19,9 @@ class AdaptiveController(app_manager.OSKenApp):
         self.logger.info("AI-SDN Adaptive Controller Started")
         self.logger.info("=" * 50)
 
+        # MAC address table
+        self.mac_to_port = {}
+
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
 
@@ -24,3 +31,100 @@ class AdaptiveController(app_manager.OSKenApp):
         self.logger.info("Switch Connected!")
         self.logger.info(f"Datapath ID : {datapath.id}")
         self.logger.info("--------------------------------")
+
+    def add_flow(self, datapath, priority, match, actions):
+
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        instructions = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                actions
+            )
+        ]
+
+        flow_mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=instructions
+        )
+
+        datapath.send_msg(flow_mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+
+        msg = ev.msg
+        datapath = msg.datapath
+        dpid = datapath.id
+
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        if eth is None:
+            return
+
+        dst = eth.dst
+        src = eth.src
+
+        # Ignore LLDP packets
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+
+        # Create MAC table for this switch
+        self.mac_to_port.setdefault(dpid, {})
+
+        # Learn source MAC address
+        self.mac_to_port[dpid][src] = in_port
+
+        self.logger.info(
+            f"Packet received at switch {dpid}: "
+            f"{src} -> {dst} on port {in_port}"
+        )
+
+        # Check whether destination MAC is known
+        if dst in self.mac_to_port[dpid]:
+
+            out_port = self.mac_to_port[dpid][dst]
+
+        else:
+
+            # Destination unknown: flood
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [
+            parser.OFPActionOutput(out_port)
+        ]
+
+        # Install flow when destination is known
+        if out_port != ofproto.OFPP_FLOOD:
+
+            match = parser.OFPMatch(
+                in_port=in_port,
+                eth_dst=dst
+            )
+
+            self.add_flow(
+                datapath,
+                10,
+                match,
+                actions
+            )
+
+        # Send packet out
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+
+        datapath.send_msg(out)
